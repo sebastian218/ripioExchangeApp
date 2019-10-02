dd39e42fbdaafb821d55f7349d54c42b9bb3ff89
Sebastian Rodero <sebastianrodero@MacBook-Pro-de-Sebastian.local>
2019-10-02 19:23:23 -0300

    xchg orders api / header

---
+++ ripioExchange/frontend/src/components/layout/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <a className="navbar-brand" href="#"><img src="../../../static/assets/imgs/ripioxchg.svg"/></a>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item ">
+        <a className="nav-link" href="#">Trade <span className="sr-only">(current)</span></a>
+      </li>
+    </ul>
+     <div className="form-inline my-2 my-lg-0">
+         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+         <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User Name
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    </div>
+  </div>
+</nav>
+        )
+    }
+}
+
+export default Header
--- ripioExchange/ripioExchange/urls.py
+++ ripioExchange/ripioExchange/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('orders.urls')),
+    path('', include('frontend.urls')),
+    path('', include('orders.urls'))
 ]
